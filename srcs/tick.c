6de9eb670f00961ad29e208a605cbf7b4467fba1
Clément Hamada <clementhamada@protonmail.com>
2019-12-19 16:20:45 +0100

    Fix speed

--- srcs/tick.c
+++ srcs/tick.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/17 17:42:59 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/17 21:32:08 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/19 16:20:29 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,11 +24,17 @@ void	start_tick(t_tick *tick)
 
 void	end_tick(t_tick *tick)
 {
-	static const float	limit = 1.0 / 60;
+	static const float	limit = 1.0 / 60.0;
 	struct timeval		now;
 
 	if (tick->delta < limit)
-		usleep((limit - tick->delta) * 1000);
-	gettimeofday(&now, NULL);
-	tick->delta = ((now.tv_sec) + (now.tv_usec) / 1000000.0) - tick->start;
+	{
+		usleep((limit - tick->delta) * 1000.0);
+		tick->delta = limit;
+	}
+	else
+	{
+		gettimeofday(&now, NULL);
+		tick->delta = ((now.tv_sec) + (now.tv_usec) / 1000000.0) - tick->start;
+	}
 }
