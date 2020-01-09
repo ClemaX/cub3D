c0c373eacbc9325b7906af99c2b703df95ed1937
Clément Hamada <clementhamada@protonmail.com>
2020-01-09 23:37:06 +0100

    Refactoring

--- srcs/canvas.c
+++ srcs/canvas.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/05 15:04:23 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/09 05:44:02 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/09 23:35:26 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -41,55 +41,54 @@ int						init_canvas(t_env *env)
 
 void					draw_tex(t_env *env, int pos_x, int z, int size)
 {
-	int 		x;
-	int			y;
+	t_ivector	draw;
 	t_ivector	start;
 	t_ivector	end;
 	t_ivector	tex;
+	t_color		c; 
 
 	start = ivector(pos_x - size / 2, (env->settings.h - size) / 2);
 	end = ivector(pos_x + size / 2, (env->settings.h + size) / 2);
-	x = (start.x < 0) ? 0 : start.x;
+	draw.x = (start.x < 0) ? 0 : start.x;
 	if (end.x >= env->settings.w)
 		end.x = env->settings.w - 1;
 	if (end.y >= env->settings.h)
 		end.y = env->settings.h - 1;
-	while (x < end.x)
+	while (draw.x < end.x)
 	{
-		if (z < env->zbuffer[x])
+		if (z < env->zbuffer[draw.x])
 		{
-			tex.x = (256 * ((x - start.x) * env->tex[SPRITE_TEX].w / size) / 256);
-			y = (start.y < 0) ? 0 : start.y;
-			while (y < end.y)
+			tex.x = (256 * ((draw.x - start.x) * env->tex[SPRITE_TEX].w / size) / 256);
+			draw.y = (start.y < 0) ? 0 : start.y;
+			while (draw.y < end.y)
 			{
-				tex.y = (((y * 256 - env->settings.h * 128 + size * 128) * env->tex[SPRITE_TEX].h) / size) / 256;
-				t_color c = get_color(env->tex[SPRITE_TEX], tex.x, tex.y);
+				tex.y = (((draw.y * 256 - env->settings.h * 128 + size * 128) * env->tex[SPRITE_TEX].h) / size) / 256;
+				c = get_color(env->tex[SPRITE_TEX], tex.x, tex.y);
 				if (c.c)
-					put_canvas(env, x, y, c);
-				y++;
+					put_canvas(env, draw.x, draw.y, c);
+				draw.y++;
 			}
 		}
-		x++;
+		draw.x++;
 	}
 }
 
 void					draw_column(t_env *env, int x, t_obstacle obs)
 {
 	const int	height = env->settings.h / obs.distance;
-	int	start;
-	int	end;
-	int	y;
-	int tex_y;
+	int			end;
+	int			y;
+	int 		tex_y;
 
-	start = -height / 2 + env->settings.h / 2;
-	end = height / 2 + env->settings.h / 2;
-	if (start < 0)
-		start = 0;
+	end = -height / 2 + env->settings.h / 2;
+	if (end < 0)
+		end = 0;
+	y = 0;
+	while (y < env->settings.h && y < end)
+		put_canvas(env, x, y++, env->settings.color_c);
+	end += height;
 	if (end >= env->settings.h)
 		end = env->settings.h - 1;
-	y = 0;
-	while (y < env->settings.h && y < start)
-		put_canvas(env, x, y++, env->settings.color_c);
 	while (y < end)
 	{
 		tex_y = (((y * 256 - env->settings.h * 128 + height * 128) * env->tex[obs.face].h) / height) / 256;
